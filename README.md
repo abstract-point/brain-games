c7057480a7885d5c052be183c2662bcf9b466820
Ivan-Lysenko <diparon@yandex.ru>
2023-07-12 14:47:24 +0300

    Add BrainProgression game code with dependencies and asciinema record

--- README.md
+++ README.md
@@ -8,3 +8,5 @@
 [![asciicast](https://asciinema.org/a/595738.svg)](https://asciinema.org/a/595738)
 ### Asciinema BrainGcd game
 [![asciicast](https://asciinema.org/a/596284.svg)](https://asciinema.org/a/596284)
+### Asciinema BrainProgression game
+[![asciicast](https://asciinema.org/a/596300.svg)](https://asciinema.org/a/596300)
---
+++ src/Games/BrainProgression.php
@@ -0,0 +1,42 @@
+<?php
+namespace Php\Project\Games\BrainProgression;
+
+use function Php\Project\Engine\runGame;
+
+use function cli\line;
+use function cli\prompt;
+
+function makeProgression()
+{
+    $progression = [];
+    $progressionStart = rand(1, 20);
+    $progressionStep = rand(2, 6);
+    $lengtProgression = 10;
+    $i = 1;
+
+    while ($i < $lengtProgression) {
+        $progression[$i] = $progressionStart + $progressionStep * $i;
+        $i++;
+    }
+    return $progression;
+}
+
+function runBrainProgression()
+{
+    $gameGreeting = 'What number is missing in the progression?';
+    $gameData = [];
+    $countRightAnswerForWin = 3;
+    $counterOfRightAnswer = 0;
+
+    while ($counterOfRightAnswer < $countRightAnswerForWin) {
+        $progressionArray = makeProgression();
+        $indexMissingNum = rand(3, 9);
+        $rightAnswer = (string) $progressionArray[$indexMissingNum];
+        $progressionArray[$indexMissingNum] = '..';
+        $progressionString = implode(' ', $progressionArray);
+        $question = "Question: {$progressionString}";
+        $gameData[$question] = $rightAnswer;
+        $counterOfRightAnswer++;
+    }
+    runGame($gameGreeting, $gameData);
+}
